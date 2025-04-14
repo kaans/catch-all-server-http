9fac118a07b8eca790bc06543eaae67e594cfd11
Jens <jens.saalmueller@codecentric.de>
2025-04-14 09:33:41 +0200

    fix: parse body and prettify output

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,110 @@
-use actix_web::{App, HttpRequest, HttpServer, Responder, Result, get, post, web};
+use actix_web::{
+    App, HttpRequest, HttpResponse, HttpServer, Result, delete, error, get, options, post, put, web,
+};
+use chrono::serde::ts_seconds;
+use futures::StreamExt;
 use maud::{Markup, html};
-use serde::Serialize;
+use serde::{Serialize, Serializer};
 use std::collections::BTreeMap;
+use std::ops::Deref;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(monitor).service(monitor_post).service(monitor_json))
-        .bind(("127.0.0.1", 8092))?
-        .run()
-        .await
+    println!("Starting catch-all server on port 8092");
+    HttpServer::new(|| {
+        App::new()
+            .service(monitor_get)
+            .service(monitor_post)
+            .service(monitor_put)
+            .service(monitor_delete)
+            .service(monitor_options)
+    })
+    .bind(("127.0.0.1", 8092))?
+    .run()
+    .await
+}
+
+fn serialize_bytes<S>(bytes: &web::BytesMut, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    serializer.serialize_str(String::from_utf8_lossy(bytes.to_vec().deref()).deref())
 }
 
 #[derive(Debug, Serialize)]
 struct Request {
+    #[serde(with = "ts_seconds")]
+    time: chrono::DateTime<chrono::Utc>,
     url: String,
     path: String,
     method: String,
     query: BTreeMap<String, String>,
     headers: BTreeMap<String, String>,
     cookies: BTreeMap<String, String>,
-    body: Option<String>,
+    #[serde(serialize_with = "serialize_bytes")]
+    body: web::BytesMut,
 }
 
-#[post("/")]
-async fn monitor_post(req: HttpRequest, body: String) -> Result<Markup> {
-    println!("Incoming request: {:?}", req);
+const MAX_SIZE: usize = 262_144;
 
-    let result = parse_request(req, Some(body));
-
-    get_web_response(result)
+#[post("/{path:.*}")]
+async fn monitor_post(req: HttpRequest, body: web::Payload) -> Result<HttpResponse> {
+    parse_incoming_request(req, body).await
 }
 
-#[get("/")]
-async fn monitor(req: HttpRequest) -> Result<Markup> {
-    println!("Incoming request: {:?}", req);
-
-    let result = parse_request(req, None);
-
-    get_web_response(result)
+#[get("/{path:.*}")]
+async fn monitor_get(req: HttpRequest, body: web::Payload) -> Result<HttpResponse> {
+    parse_incoming_request(req, body).await
 }
 
-fn get_web_response(result: Request) -> Result<Markup> {
-    Ok(html! {
+#[put("/{path:.*}")]
+async fn monitor_put(req: HttpRequest, body: web::Payload) -> Result<HttpResponse> {
+    parse_incoming_request(req, body).await
+}
+
+#[delete("/{path:.*}")]
+async fn monitor_delete(req: HttpRequest, body: web::Payload) -> Result<HttpResponse> {
+    parse_incoming_request(req, body).await
+}
+
+#[options("/{path:.*}")]
+async fn monitor_options(req: HttpRequest, body: web::Payload) -> Result<HttpResponse> {
+    parse_incoming_request(req, body).await
+}
+
+async fn parse_incoming_request(req: HttpRequest, body: web::Payload) -> Result<HttpResponse> {
+    println!("=== Incoming request\n");
+
+    let request = parse_request(req, body).await?;
+
+    print_request(&request);
+
+    Ok(match request.headers.get("accept") {
+        Some(content_type) if content_type.contains("application/json") => {
+            HttpResponse::Ok().json(web::Json(request))
+        }
+        None | Some(_) => HttpResponse::Ok()
+            .content_type("text/html")
+            .body(get_web_response(request)),
+    })
+}
+
+fn print_request(request: &Request) {
+    println!("{} {}", request.method, request.url);
+    println!("Path: {}", request.path);
+    print!("Query parameters:\n{}", request.query.iter().map(|(k, v)| format!("\t{} => {}\n", k.deref(), v.deref())).collect::<String>());
+    print!("Headers:\n{}", request.headers.iter().map(|(k, v)| format!("\t{} => {}\n", k.deref(), v.deref())).collect::<String>());
+    print!("Cookies:\n{}", request.cookies.iter().map(|(k, v)| format!("\t{} => {}\n", k.deref(), v.deref())).collect::<String>());
+    println!("Body ({} bytes):\n{}", request.body.len(), String::from_utf8_lossy(request.body.to_vec().deref()));
+}
+
+fn get_web_response(result: Request) -> Markup {
+    html! {
         html {
             body {
                 h1 { "Incoming request" }
 
+                p { b {"Timestamp: "} (format!("{}", result.time.format("%Y-%m-%d %H:%M:%S%.3f"))) }
                 p { b {"Path: "} (result.path) }
                 p { b {"Method: "} (result.method) }
 
@@ -70,29 +129,22 @@ fn get_web_response(result: Request) -> Result<Markup> {
                     }
                 }
 
-                @if result.body.is_some() {
-                    p { b {"Body: "} (result.body.unwrap()) }
+                @if result.body.len() > 0 {
+                    p { b {"Body: "} (String::from_utf8_lossy(result.body.to_vec().deref())) }
                 }
             }
         }
-    })
+    }
 }
 
-#[get("/json")]
-async fn monitor_json(req: HttpRequest) -> Result<impl Responder> {
-    println!("Incoming request: {:?}", req);
-
-    let result = parse_request(req, None);
-
-    Ok(web::Json(result))
-}
-
-fn parse_request(req: HttpRequest, body: Option<String>) -> Request {
+async fn parse_request(req: HttpRequest, mut payload: web::Payload) -> Result<Request> {
     let query = req
         .query_string()
         .split("&")
+        .filter(|q| !q.is_empty())
         .map(|q| {
             let mut s = q.split("=");
+
             return (
                 s.next().unwrap_or("unknown").to_string(),
                 s.next().unwrap_or("unknown").to_string(),
@@ -100,7 +152,18 @@ fn parse_request(req: HttpRequest, body: Option<String>) -> Request {
         })
         .collect();
 
+    let mut body = web::BytesMut::new();
+    while let Some(chunk) = payload.next().await {
+        let chunk = chunk?;
+        // limit max size of in-memory payload
+        if (body.len() + chunk.len()) > MAX_SIZE {
+            return Err(error::ErrorBadRequest("overflow"));
+        }
+        body.extend_from_slice(&chunk);
+    }
+
     let result = Request {
+        time: chrono::Utc::now(),
         url: req.uri().to_string(),
         path: req.path().to_string(),
         method: req.method().to_string(),
@@ -120,5 +183,6 @@ fn parse_request(req: HttpRequest, body: Option<String>) -> Request {
             .unwrap_or_default(),
         body,
     };
-    result
+
+    Ok(result)
 }
